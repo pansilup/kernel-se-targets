246343de77d8eef77e5e34ad357c73500340a05e
pansilup <pansilup@smu.edu.sg>
2023-01-05 13:42:13 +0800

    ading new targets

---
+++ s2e/dup3.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+
+static __attribute__ ((noinline)) unsigned long long rdtsc(void)
+{
+    unsigned hi, lo;
+    asm volatile ("rdtsc" : "=a"(lo), "=d"(hi));
+    return ((unsigned long long) lo | (unsigned long long) hi << 32);
+}
+
+int main (void)
+{
+    int ret;
+    int flags = 524288; //##symbol
+
+    //unsigned long t0 = rdtsc();
+    asm volatile("movq $292, %%rax; \n\t"
+            "movq $2, %%rdi; \n\t"
+            "movq $5, %%rsi; \n\t"	    
+            "movq %1, %%rdx; \n\t"	    
+            "syscall; \n\t"
+            "movq %%rax, %0; \n\t"
+            :"=m"(ret):"m"(flags):"%rax","%rdi","%rdx");
+    //unsigned long t1 = rdtsc();
+  
+    //printf ("ret of dup3: %d \n", ret);
+    //printf ("ret of getpriority: %d  cy : %lu\n", ret, t1-t0);
+    
+    return 1;
+}
